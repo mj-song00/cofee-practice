8dab752bb5971e4e33d95f07b24865228ddf00f4
Jason <timeli@naver.com>
2022-06-10 19:35:30 +0900

    데이터베이스 연결을 위한 패키지 설치

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
+const postRouter = require('./routes/post');
 const app = express();
+
+app.get('/', (req, res, next) => {
+  res.send('Hello');
+});
+
+app.use('/api', postRouter);
 
 app.listen(8000, () => {
   console.log('8000번 서버 실행 중');
