81e964a3d4a09784a84008d21ec537e2deb7e27e
Jason <timeli@naver.com>
2022-06-11 10:34:10 +0900

    에러 핸들러 추가

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const postRouter = require('./routes/post');
 const db = require('./models');
 const cors = require('cors');
-const morgan = require('morgan');
+const morgan = require('morgan'); // 터미널에 프론트 요청오면 로그 찍합니다.
+
+const postRouter = require('./routes/post');
 
 const app = express();
 
@@ -21,6 +22,15 @@ app.get('/', (req, res, next) => {
 });
 
 app.use('/api', postRouter);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.sendStatus(500);
+});
 
 app.listen(8000, () => {
   console.log('8000번 서버 실행 중');
