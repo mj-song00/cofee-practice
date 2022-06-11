198317e3de876e4e4f527ed3bcf70dd70a8c8007
Jason <timeli@naver.com>
2022-06-11 11:26:03 +0900

    게시글 조회, 작성 라우터 추가

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,32 @@
 const express = require('express');
+const { User, Post } = require('../models');
 
 const router = express.Router();
+
+// 전체게시글 조회
+router.get('/posts', async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      order: [['createdAt', 'DESC']],
+      include: [{ model: User }],
+    });
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// 게시글 작성
+router.post('/post', async (req, res, next) => {
+  const { title, img, content } = req.body;
+  try {
+    const post = await Post.create({ title, img, content });
+    res.status(201).json(post);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 module.exports = router;
